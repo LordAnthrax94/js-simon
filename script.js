fa31ca2c6fde90aa05e2ca15d24fdb80cd99f288
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-26 15:33:30 +0100

    fix: impostato timer

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let conteggio = document.getElementById('display')
-let counter = 10 ;
+const conteggio = document.getElementById('display');
+const selettorenum = document.querySelector('.schedanum');
+let counter = 11 ;
+const bottone = document.getElementById('btn');
 
-const timer = setInterval(cronotimer, 10000);
-
-function cronotimer(){
-  conteggio.innerHTML = ;
+const timer = setInterval(function(){
   counter--;
-}
+  document.getElementById('display').innerHTML = counter;
+  console.log(counter);
+
+  if(counter == 0){
+    clearInterval(timer)
+    selettorenum.classList.add('d-hide')
+    bottone.classList.remove('d-hide')
+  }
+},1000)
+
+
+
+
+
+
 
