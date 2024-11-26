e2c3ee00d7c27e02b0826daff8ecaebccf14815b
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-26 19:10:08 +0100

    fix: aggiunta funzione

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const conteggio = document.getElementById('display');
 const selettorenum = document.querySelector('.schedanum');
-let counter = 11 ;
-const bottone = document.querySelector('.btn');
+let counter = 2 ;
+const numform = document.getElementById('insnumeri');
 
 let numerirandom = [];
 
@@ -10,7 +10,7 @@ const max = 50;
 
  
 for(let i = 0; i < 5; i++ ){
-  numerirandom.push(Math.floor(Math.random((max - min) + min)* 50))
+  numerirandom.push(Math.floor(Math.random((max - min) + min)* 50))  
 }
 
 console.log(numerirandom.toString())
@@ -24,12 +24,47 @@ const timer = setInterval(function(){
   if(counter == 0){
     clearInterval(timer)
     
-    document.querySelector('.insnumeri').classList.remove('d-none')
-    document.querySelector('.btn').classList.remove('d-none')
+    document.getElementById('insnumeri').classList.remove('d-none')
+   // document.querySelector('.btn').classList.remove('d-none')
     document.getElementById('selezrndm').classList.add('d-none')
   }
 },1000)
 
+    
+    
+
+
+numform.addEventListener('submit', (event) => { 
+  event.preventDefault() 
+   const num1 = document.getElementById('num1')
+   const num2 = document.getElementById('num2')
+   const num3 = document.getElementById('num3')
+   const num4 = document.getElementById('num4')
+   const num5 = document.getElementById('num5')
+
+    let numinseriti=[
+      parseInt(num1.value), 
+      parseInt(num2.value), 
+      parseInt(num3.value), 
+      parseInt(num4.value), 
+      parseInt(num5.value)
+    ]
+
+    function validazione(array1, array2){
+      return array1.filter(number => array2.includes(number))
+    }
+
+    let numvalidati = validazione(numinseriti, numerirandom);
+
+    let x = numvalidati.length   
+    
+    confirm(x, numvalidati)
+   
+})
+
+function confirm(confirm1, confirm2){
+  let indovinati = confirm(x, numvalidati)  
+}
 
 
 
@@ -38,7 +73,15 @@ const timer = setInterval(function(){
 
 
 
-;
+
+
+
+
+
+
+
+
+
 
 
 
