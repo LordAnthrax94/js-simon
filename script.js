733ff6017ecdc24f650e92883ffbda036fc3b5f9
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-26 15:13:33 +0100

    fit: inserito README, index, css e script

---
+++ script.js
@@ -0,0 +1,10 @@
+let conteggio = document.getElementById('display')
+let counter = 10 ;
+
+const timer = setInterval(cronotimer, 10000);
+
+function cronotimer(){
+  conteggio.innerHTML = ;
+  counter--;
+}
+
