91bd1cec1500c68bd52a35d125f150cf0ca37fe5
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-26 16:38:04 +0100

    fix: inserito ciclo for per generare numeri random

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const conteggio = document.getElementById('display');
 const selettorenum = document.querySelector('.schedanum');
 let counter = 11 ;
-const bottone = document.getElementById('btn');
+const bottone = document.querySelector('.btn');
+
+let numerirandom = [];
+
+const min = 1;
+const max = 50;
+
+ 
+for(let i = 0; i < 5; i++ ){
+  numerirandom.push(Math.floor(Math.random((max - min) + min)* 50))
+}
+
+document.getElementById('selezrndm').innerHTML = numerirandom;
 
 const timer = setInterval(function(){
   counter--;
@@ -10,10 +22,22 @@ const timer = setInterval(function(){
 
   if(counter == 0){
     clearInterval(timer)
-    selettorenum.classList.add('d-hide')
-    bottone.classList.remove('d-hide')
+    document.querySelector('.schedanum').classList.remove('d-none')
+    document.querySelector('.btn').classList.remove('d-none')
+    document.getElementById('selezrndm').classList.add('d-none')
   }
 },1000)
+
+
+
+
+
+
+
+console.log(numerirandom);
+
+
+
 
 
 
